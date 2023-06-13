810136ddb2caf502bb5706e2ecd2e7ce43ba8a11
amir sohail <9902699141a@gmail.com>
2023-06-13 12:24:21 +0530

    Chance base winning logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import './App.css' ;
 import Blastoise from './images/blastoise.png' ;
 import Charizard from './images/charizard.png' ;
@@ -9,22 +9,30 @@ import Pokemon from './Pokemon';
 const App = () => {
 
     // react hooks
-    const [blastoiseHP, setBlastoisedHP ] = useState(120)
-    const [charizardHP, setCharizardHP ] = useState(100)
+    const [blastoiseHP, setBlastoisedHP ] = useState(110)
+    const [charizardHP, setCharizardHP ] = useState(120)
+
+    // Random Num generate
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
 
     
-    // Functions
+    // Functions attack
     const blastoiseAttack = ()=>
     {
-        setCharizardHP(charizardHP-30)
+        const num1 = getRandomNumber(15, 30);
+        setCharizardHP(charizardHP- num1)
     }
 
     
     const charizardAttack = ()=>
     {
-        setBlastoisedHP(blastoiseHP-40)
+        const num1 = getRandomNumber(15, 30);
+        setBlastoisedHP(blastoiseHP-num1)
     }
 
+    //  Win Condition
     const container = document.querySelector('.container')
 
     if(charizardHP<=0)
